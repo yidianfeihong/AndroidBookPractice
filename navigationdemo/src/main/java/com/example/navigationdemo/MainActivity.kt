f9e5d130930cebd60c551102a035e2266e3f1ff5
ming <1099155429@qq.com>
2025-09-06 00:29:54 +0800

    add simple jetpack navigation demo

---
+++ navigationdemo/src/main/java/com/example/navigationdemo/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.navigationdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
+import com.example.navigationdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportFragmentManager.findFragmentById(R.id.nav_host)?.apply {
+            val controller = findNavController()
+            binding.changeFragmentButton.setOnClickListener {
+                controller.navigate(R.id.action_first_to_second, Bundle().apply {
+                    putString("title", "test title")
+                    putString("desc", "test desc")
+                })
+            }
+        }
+    }
+}
---
+++ navigationdemo/src/main/java/com/example/navigationdemo/fragment/SecondFragment.kt
@@ -0,0 +1,45 @@
+package com.example.navigationdemo.fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigationdemo.R
+
+private const val ARG_PARAM1 = "title"
+private const val ARG_PARAM2 = "desc"
+
+class SecondFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+            Log.d("SecondFragment", "onCreate get params:$param1 $param2")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SecondFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
